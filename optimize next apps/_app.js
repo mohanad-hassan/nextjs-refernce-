081774b3a747c44c1d01bf8498653429d333f523
mohanad-hassan <110348639+mohanad-hassan@users.noreply.github.com>
2023-04-21 11:06:28 +0300

    Create _app.js

---
+++ optimize next apps/_app.js
@@ -0,0 +1,6 @@
+// this file is the root app content wich will renders every compnent in the project .
+//you can wrap a navbar for the whole project in this file .
+//the Head you set here will be used for the whole project .
+//when you rewrite the Head in normal pages it will MERGE them and  rewrite the same attributes and tags  Head  in _app.js file .
+//if you set more than one Head in the same page it will merge them but the last one is more powerfull .
+//
